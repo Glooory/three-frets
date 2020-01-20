7bbe38c56c7e358238f20c2c8dc2d04d7b3c78d2
Glooory <glooorypu@gmail.com>
2020-01-20 16:28:47 +0800

    Init commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './home';
+
+const App: React.FC = () => (
+  <Router>
+    <Switch>
+      <Route path="/" component={Home} />
+    </Switch>
+  </Router>
+);
+
+export default App;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './home.module.scss';
+
+const Home: React.FC = () => <div className={styles.home}>Home</div>;
+
+export default Home;
